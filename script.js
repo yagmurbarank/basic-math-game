663f9c9811c7e946870d99201565bf71b75accf7
yagmurbrnk <yagmurbarankarakus@outlook.com>
2023-11-12 23:47:07 +0300

    add files

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict"
+// Start writing code from here
+// You should write code for step 3 from here.
+// Declare all the global variables
+
+const generateNumber = (lower, upper) => {
+    // Generates a random number which is present in between lower bound and upper bound.
+    // You should complete this function here
+    
+}
+const generateOperator = () => {
+    // Generates a random operator which is present in arrayOfOperator.
+    // You should complete this function here
+    
+}
+
+// You should write code for step 4 from here.
+const generateQuestion = () => {
+    // sets the value of `operand1`, `operator` and `operand2` fields.
+    // You should complete this function here
+
+}
+
+const generateAnswer = () => {
+    // You should complete this function here
+
+}
+// You should write code for step 5 from here.
+
+// You should call the generateAnswer() function here
